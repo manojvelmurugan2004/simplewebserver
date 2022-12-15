8a66a0e90a125066576a347c6b9b73b2e4eb8a22
manojvelu2004@gmail.com <manojvelu2004@gmail.com>
2022-12-15 14:32:25 +0000

    success

--- manoj.py
+++ manoj.py
@@ -1,21 +1,4 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
 <html>
@@ -38,11 +21,3 @@ server_address = ('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-```
-
-
-## OUTPUT:
-![OUTPUT](./images/manoj.png)
-
-## RESULT:
-Program is executed successfully
